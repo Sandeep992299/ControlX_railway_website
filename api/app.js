3fa8c3a58041ac4f6addc6fd40841938e787bdfb
Kaveesha Senarathne <kaveeshagimhan49@gmail.com>
2025-08-12 09:15:44 +0530

    Update express port to :5050

--- api/app.js
+++ api/app.js
@@ -2,13 +2,13 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5050;
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/api/hello', (req, res) => {
-  res.json({ message: 'Hello from Express server!' });
+app.get("/api/hello", (req, res) => {
+  res.json({ message: "Hello from Express server!" });
 });
 
 app.listen(PORT, () => {
